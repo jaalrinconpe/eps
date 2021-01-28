bb43ec3c30f1b28101f37e8d66aa8c953cf40010
Javier <jaalrinconpe@gmail.com>
2021-01-27 20:25:20 -0500

    Corregir pathparam

--- src/main/java/co/eps/controller/AfiliadoController.java
+++ src/main/java/co/eps/controller/AfiliadoController.java
@@ -1,11 +1,10 @@
 package co.eps.controller;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +26,8 @@ public class AfiliadoController {
 	}
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Afiliado buscarPorId(@PathVariable("id") Long id) {
-		return afiliadoService.buscarPorId(id);
+	public Afiliado buscarPorId(@PathVariable("id") Long idAfiliado) {
+		return afiliadoService.buscarPorId(idAfiliado);
 	}
 	
 	@PostMapping(value = "/guardar", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -37,7 +36,7 @@ public class AfiliadoController {
 	}
 	
 	@DeleteMapping(value = "/eliminar/{id}")
-	public void eliminar(@PathParam("id") Long id) {
+	public void eliminar(@PathVariable("id") Long id) {
 		afiliadoService.eliminar(id);
 	}
 
